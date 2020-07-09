f610c1082709860f00118b5cad274af8e08ac8ef
Hermes Luciano Monteiro Junior <you@example.com>
2020-07-09 20:49:02 -0300

    alteracao da permissao de cors

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://salty-plains-16987.herokuapp.com/"
+    origin: "https://salty-plains-16987.herokuapp.com"
   })
 );
 
