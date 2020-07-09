a12684905378f6287b6f8f56b9bf1e8a6413783a
Hermes Luciano Monteiro Junior <you@example.com>
2020-07-09 20:37:54 -0300

    alteracao da permissao de cors

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: "https://guarded-plateau-48463.herokuapp.com/"
   })
 );
 
